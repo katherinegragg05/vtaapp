747125cfb634d6ffd3b4a20323e5569646a22933
katherinegragg05 <katherinegragg05@gmail.com>
2022-10-13 16:09:08 +0800

    REmove all

--- server/main.js
+++ server/main.js
@@ -1,5 +1,18 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { TasksCollection } from '/imports/api/TasksCollection';
+
+
+
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
 });
