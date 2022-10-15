38625b5af5a68ba4afe9b0a4271306b0f46dc36c
Steven Gragg <devsteve00@gmail.com>
2022-10-16 06:32:30 +0800

    Initialize client and server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 import * as dotenv from "dotenv";
 dotenv.config();
+import express from "express";
+import cors from "cors";
 
 // config
-import connectToMongoDB from "./config/database";
-import startApolloServer from "./config/startApolloServer";
+import connectToMongoDB from "./config/database.js";
+import authRouter from "./routes/auth.js";
+import userRouter from "./routes/users.js";
 
 // connect to database first
 connectToMongoDB();
 
-// start apollo server
-startApolloServer();
+// express
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
+
+const port = process.env.PORT || 8080;
+//start the server
+app.listen(port, console.log(`🚀 Listening on port ${port}...`));
